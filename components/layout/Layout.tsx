a16711935c9d6ba13f1f737f4c3dc08d370d7078
shuh <shuh47@yandex.ru>
2022-03-01 14:04:16 +0300

    delete styled

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Header } from "@components/header/Header";
 
 export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div className="app">
+    <div>
       <Header />
       <main>{children}</main>
     </div>
