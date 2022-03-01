6d5ad2b4c57cafb54ef66d3d1d7eeeaa5afa1555
shuh <shuh47@yandex.ru>
2022-03-01 14:21:08 +0300

    delete styled

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Logo } from "@components/header/logo/Logo";
 
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Logo />
+    </header>
+  );
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
+module.exports = nextConfig;
